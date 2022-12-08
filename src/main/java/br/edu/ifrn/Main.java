6587a9b4998a94148fbc19392330ad956b2a966c
leirdan <uouleirdan@gmail.com>
2022-12-08 14:14:37 -0300

    Criando classe de usuário e coleta dos dados

--- src/main/java/br/edu/ifrn/Main.java
+++ src/main/java/br/edu/ifrn/Main.java
@@ -1,11 +1,33 @@
 package br.edu.ifrn;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) throws InputMismatchException {
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.print("Qual o seu nome? ");
+            String name = sc.nextLine();
+
+            System.out.printf("%s, qual é a sua altura?", name);
+            Double height = sc.nextDouble();
+
+            System.out.printf("Por fim, %s, qual o seu peso?", name);
+            Double weight = sc.nextDouble();
+
+            final User user = new User(name, height, weight);
+
+            final var imc = calculate(user.getHeight(), user.getWeight());
+
+        } catch (InputMismatchException err) {
+            throw new InputMismatchException("O valor não deve ser digitado com um '.', apenas com ','.");
+        } finally {
+            sc.close();
+        }
     }
 
-    public Integer calculate() {
+    public static Integer calculate(Double h, Double w) {
         return 0;
     }
 
--- src/main/java/br/edu/ifrn/User.java
+++ src/main/java/br/edu/ifrn/User.java
@@ -1,5 +1,26 @@
 package br.edu.ifrn;
 
 public class User {
+    protected String name;
+    private Double height;
+    private Double weight;
+
+    public User(String name, Double height, Double weight) {
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    protected String getName() {
+        return this.name;
+    }
+
+    protected Double getHeight() {
+        return this.height;
+    }
+
+    protected Double getWeight() {
+        return this.weight;
+    }
 
 }
